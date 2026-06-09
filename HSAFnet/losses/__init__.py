c0980f58b8c58fe3d3570efdddbdc188bd8ebb27
gdiand <2766932185@qq.com>
2026-06-09 21:18:34 +0800

    Delete HSAFnet directory

--- HSAFnet/losses/__init__.py
+++
@@ -1,4 +0,0 @@
-from .NT_Xent import NT_Xent
-from .NT_Xent_dist import NT_Xent_dist
-
-from .build import build_loss
--- HSAFnet/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .resnet import ResNet
-from .moco import MoCo
-from .simsiam import SimSiam
-from .byol import BYOL
-
-from .build import build_model
